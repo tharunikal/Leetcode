5463e5b3997cfbf475db308864b0f9f128d0b2c7
Tharunika L <tharunika.l14@gmail.com>
2023-06-18 23:02:34 +0530

    Create 58.Length of last word.cpp

---
+++ 58.Length of last word.cpp
@@ -0,0 +1,22 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip any trailing spaces
+        while (i >= 0 && s[i] == ' ')
+            i--;
+
+        // Count the length of the last word
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+};
