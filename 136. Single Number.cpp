d6bbb3db9cb693dade8231c771eb3200ba2d1bd7
Tharunika L <tharunika.l14@gmail.com>
2023-06-24 21:08:55 +0530

    Create 136. Single Number.cpp

---
+++ 136. Single Number.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+class Solution {
+public:
+    int singleNumber(std::vector<int>& nums) {
+        int result = 0;
+        
+        for (int num : nums) {
+            result ^= num;
+        }
+        
+        return result;
+    }
+};
