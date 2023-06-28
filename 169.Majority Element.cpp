cd4a8642def40cd34254641e9139e9e70796ac57
Tharunika L <tharunika.l14@gmail.com>
2023-06-28 19:56:14 +0530

    Create 169.Majority Element.cpp

---
+++ 169.Majority Element.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
+    }
+};
