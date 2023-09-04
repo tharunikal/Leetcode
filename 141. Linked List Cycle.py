33da04aee43f4abd2d1645a9684236d3e5f05935
Tharunika L <tharunika.l14@gmail.com>
2023-09-04 20:45:57 +0530

    Create 141. Linked List Cycle.py

---
+++ 141. Linked List Cycle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited_nodes = set()
+        current_node = head
+        while current_node:
+            if current_node in visited_nodes:
+                return True
+            visited_nodes.add(current_node)
+            current_node = current_node.next
+        return False                                                                 
