e23b1b00273b672ad013f18b42a2b14d97d8b1a8
Tharunika L <tharunika.l14@gmail.com>
2024-05-26 15:29:59 +0530

    Create Studence attendance record II.java

---
+++ Studence attendance record II.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    private static final int MOD = 1000000000 + 7;
+
+    public int checkRecord(int n) {
+        return numPossibleRecords(n, 1, 2);
+    }
+
+    private int numPossibleRecords(int n, int numAbsensesRemaining, int numLatesRemaining) {
+        if(n == 0) {
+            return 1;
+        }
+        int total = 0;
+
+        // Pick P
+        total += numPossibleRecords(n - 1, numAbsensesRemaining, 2);
+        
+        if(numAbsensesRemaining > 0) {
+            // Pick A
+            total += numPossibleRecords(n - 1, numAbsensesRemaining - 1, 2);
+            total %= MOD;
+        }
+
+        if(numLatesRemaining > 0) {
+            // Pick L
+            total += numPossibleRecords(n - 1, numAbsensesRemaining, numLatesRemaining - 1);
+            total %= MOD;
+        }
+
+        return total;
+    }
+}
