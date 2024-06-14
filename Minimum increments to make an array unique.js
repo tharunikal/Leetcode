861d537babdc3a2378067f811eb96b2e1bf57933
Tharunika L <tharunika.l14@gmail.com>
2024-06-14 10:01:17 +0530

    Create Minimum increments to make an array unique.js

---
+++ Minimum increments to make an array unique.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minIncrementForUnique = function (nums) {
+    // Sort the array first
+    nums.sort((a, b) => a - b);
+
+    let numTracker = 0;   // Tracks the next unique number that should be set.
+    let minIncrement = 0; // Counts the total increments required.
+
+    for (let num of nums) {
+        numTracker = Math.max(numTracker, num);
+        minIncrement += numTracker - num;
+        numTracker++; // Increment the tracker for the next number.
+    }
+
+    return minIncrement;
+};
