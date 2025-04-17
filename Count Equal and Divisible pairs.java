eb64e70c77c615ff86e51140b7fc8f2960c227a7
Tharunika L <tharunika.l14@gmail.com>
2025-04-17 22:36:47 +0530

    Create Count Equal and Divisible pairs.java

---
+++ Count Equal and Divisible pairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countPairs(int[] nums, int k) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j] && ((long)i * j) % k == 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
